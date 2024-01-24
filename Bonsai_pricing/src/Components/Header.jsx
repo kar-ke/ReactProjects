e6bf5c7fc2a2152cc10a6e3ec224fa0e30dceeb6
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2024-01-24 21:30:55 +0530

    bonsai project initial

---
+++ Bonsai_pricing/src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+		<nav>
+			<div>
+				<img
+					src="https://web.archive.org/web/20221117021714im_/https://assets-global.website-files.com/58868bcd2ef4daaf0f072900/5e5fd7c602ca7cd432feb68e_bonsai-logo.svg"
+					alt="logo"
+				/>
+			</div>
+			<div>
+                
+            </div>
+			<div></div>
+		</nav>
+  );
+}
+
+export default Header
---
+++ Bonsai_pricing/src/Contents/Menuitems.js
@@ -0,0 +1,73 @@
+export const navMenu = [
+	{
+		title: "Product",
+		submenu: [
+			{
+				title: "Bonsai Workflow",
+				description:
+					"Look professional, win more clients and manage your business from one place",
+				submenu: [
+					{
+						title: "Invoice",
+						description:
+							"Get paid faster with automatic invoicing, online payments and more",
+					},
+					{
+						title: "Proposals",
+						description:
+							"Create client-winning proposals with open receipts, custom packages and electronic approvals",
+					},
+					{
+						title: "Contracts",
+						description:
+							"Protect your business with fully-customizable contract templates. Edit and e-sign hassle free.",
+					},
+					{
+						title: "Client CRM",
+						description:
+							"Add leads, manage existing clients and track projects easily from one place",
+					},
+					{
+						title: "Forms & Automations",
+						description:
+							"Boost client intake and automate feedback with custom web forms and questionnaires",
+					},
+					{
+						title: "Time Tracking",
+						description:
+							"Stay organized with Bonsai's all-in-one time tracker, automated timesheets and billing system",
+					},
+					{
+						title: "Accounting",
+						description:
+							"Manage your finances easily with income reports, expense tracking, tax reminders and more",
+					},
+					{
+						title: "Task Tracking",
+						description:
+							"Stress-free task management with project templates and integrated timesheets",
+					},
+				],
+			},
+			{
+				title: "Bonsai Tax",
+				description:
+					"Track expenses, maximize tax write-offs, and estimate taxes without the headache",
+			},
+			{
+				title: "Bonsai Cash",
+				description:
+					"Bonsai's all-in-one financial hub: No fees and lightning fast payouts",
+			},
+		],
+	},
+	{
+		title: "Templates"
+	},
+	{
+		title: "Pricing"
+	},
+	{
+		title: "Reviews"
+	}
+];
